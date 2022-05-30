33b3312ec05add2dfb0d2219fb68b5f65c46f26d
miriam <miriam_glzr@hotmail.com>
2022-05-30 19:49:01 +0200

    converting images to webp

--- staticHandler.mjs
+++ staticHandler.mjs
@@ -1,73 +1,77 @@
-import { createReadStream, constants } from 'fs'
-import { stat, access } from 'fs/promises'
-import { join, sep } from 'path'
-import mime from 'mime-types'
+import { createReadStream, constants } from "fs";
+import { stat, access } from "fs/promises";
+import { join, sep } from "path";
+import mime from "mime-types";
 
-const baseDir = process.env.NODE_ENV === 'production' ? 'dist' : 'src'
+const baseDir = process.env.NODE_ENV === "production" ? "dist" : "src";
 
 export default async function staticHandler(req, res) {
-  // Parse the request URL.
-  const url = new URL(req.url, `http://${req.headers.host}`)
+	// Parse the request URL.
+	const url = new URL(req.url, `http://${req.headers.host}`);
 
-  // Clean the path.
-  const filePath = await cleanPath(url.pathname)
+	// Clean the path.
+	const filePath = await cleanPath(url.pathname);
 
-  // Check if the file exists.
-  if (filePath) {
-    // Found!
-    res.statusCode = 200
+	// Check if the file exists.
+	if (filePath) {
+		// Found!
+		res.statusCode = 200;
 
-    // Here you can set headers like Cache-Control, etc.
-    res.setHeader('Content-Type', mime.lookup(filePath) || undefined)
+		// Here you can set headers like Cache-Control, etc.
+		res.setHeader("Content-Type", mime.lookup(filePath) || undefined);
+		res.setHeader("Cache-Control", "max-age=31556926");
+		res.setHeader("Vary", "Authorization");
 
-    // Stream the file to the response.
-    createReadStream(filePath).pipe(res)
-  } else {
-    // Oh no, file does not exist.
-    res.statusCode = 404
-    res.end('404 Not found')
-  }
+		res.setHeader("Cache-Control", "max-age=60, stale-while-revalidate=60");
+
+		// Stream the file to the response.
+		createReadStream(filePath).pipe(res);
+	} else {
+		// Oh no, file does not exist.
+		res.statusCode = 404;
+		res.end("404 Not found");
+	}
 }
 
 // Clean up request path so we can use it to open a file.
 // NOTE: This is a naive implementation and should not be used in production.
 async function cleanPath(path) {
-  // Remove ".." segments
-  path = path.replace(/\/\.\.(?=\/)/g, '')
-  // Remove duplicate /
-  path = path.replace(/\/+(?=\/)/g, '')
-  // Remove starting /
-  path = path.replace(/^\/+/, '')
+	// Remove ".." segments
+	path = path.replace(/\/\.\.(?=\/)/g, "");
+	// Remove duplicate /
+	path = path.replace(/\/+(?=\/)/g, "");
+	// Remove starting /
+	path = path.replace(/^\/+/, "");
 
-  // Convert to FS path
-  path = path.split('/').join(sep)
+	// Convert to FS path
+	path = path.split("/").join(sep);
 
-  // Prepend the baseDir.
-  path = join(baseDir, path)
+	// Prepend the baseDir.
+	path = join(baseDir, path);
 
-  // Check if file exists.
-  if (await isValidFile(path)) {
-    return path
-  }
+	// Check if file exists.
+	if (await isValidFile(path)) {
+		return path;
+	}
 
-  // Check if an index file exists.
-  path = join(path, 'index.html')
-  if (await isValidFile(path)) {
-    return path
-  }
+	// Check if an index file exists.
+	path = join(path, "index.html");
+	if (await isValidFile(path)) {
+		return path;
+	}
 
-  return null
+	return null;
 }
 
 async function isValidFile(path) {
-  try {
-    // Check if we can read the file (throws an error if not).
-    await access(path, constants.R_OK)
+	try {
+		// Check if we can read the file (throws an error if not).
+		await access(path, constants.R_OK);
 
-    // Check if the path exists as a file (and not directory).
-    const fileStat = await stat(path)
-    return fileStat.isFile()
-  } catch (err) {
-    return false
-  }
+		// Check if the path exists as a file (and not directory).
+		const fileStat = await stat(path);
+		return fileStat.isFile();
+	} catch (err) {
+		return false;
+	}
 }
